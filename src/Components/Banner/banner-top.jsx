3ed5f1d3e61bddd9af9c09858eef035aa353dbf7
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2023-12-21 22:16:47 -0500

    testing layout

--- src/Components/Banner/banner-top.jsx
+++ src/Components/Banner/banner-top.jsx
@@ -27,14 +27,14 @@ const features = [
 
 function BannerTop() {
   return (
-    <div className="py-24 sm:py-24">
+    <div className="p-4">
+      <div className="absolute inset-0 -z-10 bg-gradient-to-r from-orange-300 to-indigo-300 opacity-70" />
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="absolute inset-0 -z-10 bg-gradient-to-r from-orange-300 to-indigo-300 opacity-70" />
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="text-base font-semibold leading-7 text-indigo-600">
             What Are You Waiting For? Start Browsing
           </h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          <p className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Upgrade Your Space, Upgrade Your Life
           </p>
           <p className="mt-6 text-lg leading-8 text-gray-600">
@@ -42,8 +42,8 @@ function BannerTop() {
             for swift delivery today. Elevate your surroundings instantly.
           </p>
         </div>
-        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+        <div className="mx-auto max-w-2xl">
+          <dl className="grid grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2">
             {features.map((feature) => (
               <div key={feature.name} className="relative pl-16">
                 <dt className="text-base font-semibold leading-7 text-gray-900">
@@ -55,7 +55,7 @@ function BannerTop() {
                   </div>
                   {feature.name}
                 </dt>
-                <dd className="mt-2 text-base leading-7 text-gray-600">
+                <dd className="text-base leading-7 text-gray-600">
                   {feature.description}
                 </dd>
               </div>
